2add7f95d6b832e8fb3263405c4b0a8a45ebcd9a
Imam Kurniansyah <imam.kurniansyah97@gmail.com>
2021-11-30 20:00:48 +0700

    Rename nama activity

--- app/src/main/java/com/karta/EdittampilagtActivity.java
+++ app/src/main/java/com/karta/EdittampilagtActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Edittampilagt extends AppCompatActivity {
+public class EdittampilagtActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
